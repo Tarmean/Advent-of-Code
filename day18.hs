299fac7c22c0042221594a0b938f77e757a708be
Taren <cyril-fahlenbock@hotmail.de>
2016-12-18 13:14:52 +0100

    Clean up day 18

--- day18.hs
+++ day18.hs
@@ -1,7 +1,6 @@
 import Data.List (tails)
 
 step = map ((/=) <$> head <*> last) . windows 3 . ([False] ++) . (++ [False])
-countDots n = length . filter not . concat . take n . iterate step
-solve = flip countDots . map (== '^')
-
+solve = flip countSafe . map (== '^')
+countSafe n = sum . map (length . filter not) . take n . iterate step
 windows n = takeWhile ((==n) . length) . map (take n) . tails
